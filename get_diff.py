2d78287c3d0d6534fe3c818daa66ccf5ccac5347
MajidEM <mail2majidm@gmail.com>
2025-02-17 22:55:43 +0530

    Update get_diff.py

--- get_diff.py
+++ get_diff.py
@@ -1,15 +1,9 @@
 import re,sys
 diff_text = sys.argv[1]
-# index 5a49d0c..e1497ad 100644
-# --- a/Dockerfile
-# +++ b/Dockerfile
-# @@ -1,4 +1,4 @@
-# -FROM nginx:1.25-alpine3.17-slim
-# +FROM nginx:1.25-alpine3.17-slim'''
-
-text = re.split("\n", diff_text)
-str1 = text[5].lstrip("-")
-str2  =text[6].lstrip("+")
+fh = open(diff_text, "r")
+lines = fh.readlines()
+str1 = lines[4].lstrip("-")
+str2 = lines[5].lstrip("+")
 if str1 != str2:
 	print ("Diff found in docker images, triggering in the repositories that will recieve the updates")
 else:
